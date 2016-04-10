9147e07024ff6c65a351463cf314c29d13aaf2fa
Dilan Zúniga <dylankarim96@gmail.com>
2016-04-09 20:44:18 -0600

    Actualización

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-<html>
- <head>
-  <title>Página que imprime en pantalla"Hola Mundo"</title>
- </head>
- <body>
- <?php echo '<p>Hola Mundo</p>'; ?>
- </body>
+<!doctype html>
+<html>    
+ <head>                                 <!-------Llave de apertura para título de página---------->
+  <title>Hola Mundo</title>             <!-------Título de página---------->
+ </head>                                <!-------Llave de cierre para título de página---------->
+ <body>                                 <!-------Llave de apertura para el cuerpo de la página---------->
+ <?php echo '<p>Hola Mundo</p>'; ?>     <!-------impresión en pantalla de Hola mundo---------->
+ </body>                                <!-------Llave de apertura para el cuerpo de la página---------->
 </html>
