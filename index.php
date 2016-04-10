b6fef163ead451d3a93475ea482704d697217dff
Dilan Zúniga <dylankarim96@gmail.com>
2016-04-09 20:46:16 -0600

    Actualización

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <!doctype html>
 <html>    
- <head>                                 <!-------Llave de apertura para título de página---------->
-  <title>Hola Mundo</title>             <!-------Título de página---------->
- </head>                                <!-------Llave de cierre para título de página---------->
- <body>                                 <!-------Llave de apertura para el cuerpo de la página---------->
- <?php echo '<p>Hola Mundo</p>'; ?>     <!-------impresión en pantalla de Hola mundo---------->
- </body>                                <!-------Llave de apertura para el cuerpo de la página---------->
+ <head>                                 <!--Llave de apertura para título de página-->
+  <title>Hola Mundo</title>             <!--Título de página-->
+ </head>                                <!--Llave de cierre para título de página-->
+ <body>                                 <!--Llave de apertura para el cuerpo de la página-->
+ <?php echo '<p>Hola Mundo</p>'; ?>     <!--impresión en pantalla de Hola mundo-->
+ </body>                                <!--Llave de apertura para el cuerpo de la página--->
 </html>
